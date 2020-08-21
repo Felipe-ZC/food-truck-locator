51fac44afaf1d92a91f4daba1511edf3569b4b93
Felipe-ZC <fzunigacar@gmail.com>
2020-08-21 12:42:22 -0700

    Ignore change, client calculates time instead of server

--- showOpenFoodTrucks.py
+++ showOpenFoodTrucks.py
@@ -3,33 +3,12 @@ import sys
 from utils.mobileFoodUtil import FoodTruckSchedule
 from datetime import datetime
 from pytz import timezone
-'''
-Should client application calculate time 
-or server?
-'''
-# If the host has not been provided, look in config file
-# if not self.url:
-# try:
-# config = self.__getConfig()
-# self.url = config["host"]         
-# except Exception as e: 
-# raise RuntimeError(f"Error encountered while loading config file: {e}")
-
-# def __getConfig(self):
-# scriptPath = os.path.dirname(os.path.realpath(__file__))
-# filePath = os.path.join(scriptPath, "config.json")
-# with open(filePath) as config:
-# return json.load(config)
 
 class FoodTruckFinder:
     def __init__(self):
         # To make testing easier I've hardcoded the URL, although 
         # using a config file is the right way to go!
-        try: 
-            self.fts = FoodTruckSchedule("https://data.sfgov.org/resource/jjew-r69b.json")
-        except Exception as e:
-            print(f"Error! Could not instantiate FoodTruckSchedule service.\n{e}")
-            sys.exit()
+        self.fts = FoodTruckSchedule("https://data.sfgov.org/resource/jjew-r69b.json")
 
     def formatOutput(self, trucks):
         outputStr = [f"\"{truck['applicant']}\" \"{truck['location']}\"" for truck in trucks]
@@ -39,7 +18,7 @@ class FoodTruckFinder:
         # Python and Socrata both use ints in the range [0,6] to represent days of the week 
         # but Python's week starts on Monday while Socrata starts on Sunday.
         weekday = (datetime.today().weekday() + 1) % 7 # Compute Socrata day of week
-        currHour = str(datetime.now().strftime("%H:00")) # Current hour in 24-hour format
+        currHour = str(datetime.now().strftime("%H:%M")) # Current hour in 24-hour format
         return weekday, currHour 
     
     def getTrucksOpenNow(self, limit, offset, tz=""):
@@ -57,8 +36,6 @@ class FoodTruckFinder:
             if userIn == 'n':                
                 print("Fetching food trucks...\n")
                 try:
-                    # Use system time by default...
-                    # nextRows = self.fts.getOpenTrucksNow(limit, currPage)
                     nextRows = self.getTrucksOpenNow(limit, currPage)
                     if not nextRows or len(nextRows) < limit:
                         print("There are no more food trucks open right now.")
--- utils/mobileFoodUtil.py
+++ utils/mobileFoodUtil.py
@@ -1,13 +1,12 @@
 import requests
 import json
-import os
 from requests.utils import requote_uri
 
 class FoodTruckSchedule:
     def __init__(self, apiHost=""):
         self.url = apiHost
-     
-    def getTrucksOpenAt(self, limit, offset, day, hour):
+
+    def getTrucksOpenAt(self, limit, offset, day, time):
         '''
         Returns all food truck names and addresses that are currently open 
         at the given day and hour. Results are sorted by food truck name.
@@ -16,14 +15,14 @@ class FoodTruckSchedule:
                 limit (int): Max. number of rows to return
                 offset (int): Offset count into the results
                 day (int): An integer ranging from [0,6] representing the day of the week (Sunday = 0, Monday = 1, etc.)
-                hour: (string): A string representation of the current time in 24 hour format ("%H:00")
+                time (string): A string representation of the current time, that is, the current hour and minutes ("%H:%M")
 
             Returns:
                 data (list): A list of dicts that contains the name and address of each food truck open at the given time.
         '''
         # Implicit concatenation...
         query = (f"$select=applicant, location &"
-                 f"$where=dayorder={day} and '{hour}' >= start24 and '{hour}' < end24 &"
+                 f"$where=dayorder={day} and '{time}' >= start24 and '{time}' < end24 &"
                  f"$limit={limit} &"
                  f"$offset={offset} &"
                  f"$order=applicant")
@@ -42,7 +41,8 @@ class FoodTruckSchedule:
         '''
         reqUrl = self.url + "?" + query 
         data = None
-        response = requests.get(requote_uri(reqUrl)) # Always encode request uri as specified in Socrata's API docs
+        # Always encode request uri as specified in Socrata's API docs
+        response = requests.get(requote_uri(reqUrl)) 
         if response.status_code == 200: data = response.json()
         else: response.raise_for_status()
         return data
